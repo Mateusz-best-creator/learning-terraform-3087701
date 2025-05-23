668b1d7e7c4c7b2121d65dce8f6b00bd92978fd0
Mateusz Wieczorek <114943990+Mateusz-best-creator@users.noreply.github.com>
2025-05-23 19:24:29 +0200

    correct listener

--- main.tf
+++ main.tf
@@ -57,27 +57,6 @@ module "blog_alb" {
     bucket = "my-alb-logs"
   }
 
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-    ex-https = {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
-      forward = {
-        target_group_key = "ex-instance"
-      }
-    }
-  }
-
   target_groups = {
     ex-instance = {
       name_prefix      = "blog-"
